180c58b338a9566d275b14ffd190f86b2a07ed06
saianita <50478345+saianita@users.noreply.github.com>
2022-11-05 21:37:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './pages/Homepage';
+import {BrowserRouter ,Route,Routes} from 'react-router-dom'
+import ProductScreen from './pages/ProductScreen';
+import ShoppingCart from './pages/ShoppingCart';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Shipping from './pages/Shipping';
+import Payment from './pages/Payment';
+import PlaceOrder from './pages/PlaceOrder';
+import PostOrder from './pages/PostOrder';
+import Profile from './pages/Profile';
+import AdminUserList from './pages/AdminUserList';
+import AdminEditUser from './pages/AdminEditUser';
+import AdminProductList from './pages/AdminProductList';
+import AdminCreateProduct from './pages/AdminCreateProduct';
+import AdminOrdersList from './pages/AdminOrdersList';
+import CategoryPage from './pages/CategoryPage';
+import ScrollToTop from './components/ScrollToTop';
+import Category from './pages/Category';
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <ScrollToTop/>
+        <Routes>
+          <Route path="/" element={<Homepage/>} />      
+          <Route path="/product/:id" element={<ProductScreen/>} /> 
+          <Route path="/login" element={<Login/>} />
+          <Route path="/cart/:id" element={<ShoppingCart/>} />      
+          <Route path="/cart" element={<ShoppingCart/>} />      
+          <Route path="/signup" element={<Signup/>} />     
+          <Route path="/shipping" element={<Shipping/>} />     
+          <Route path="/payment" element={<Payment/>} />     
+          <Route path="/placeorder" element={<PlaceOrder/>} />     
+          <Route path="/profile" element={<Profile/>} />     
+          <Route path="/category" element={<CategoryPage/>} />     
+          <Route path="/order/:id" element={<PostOrder/>} />     
+          <Route path="/admin" element={<AdminUserList/>} />      
+          <Route path="/admin/productlist" element={<AdminProductList/>} />      
+          <Route path="/admin/user/:id/edit" element={<AdminEditUser/>} />      
+          <Route path="/admin/product/:id/edit" element={<AdminCreateProduct/>} />      
+          <Route path="/admin/orderlist" element={<AdminOrdersList/>} />      
+        </Routes>
+      </BrowserRouter> 
     </div>
   );
 }
